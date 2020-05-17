eea73be7c8dcb9a369f70974c288bb7cafc6475d
Night9Knight <49943191+Night9Knight@users.noreply.github.com>
2020-05-17 13:34:26 +0800

    Updated functions
    - Change staff_record_handler to return
      dict
    - Added initial function for Candidate
      and GA
    - Added randomize venue function

--- 244_Assn2.py
+++ 244_Assn2.py
@@ -1,4 +1,5 @@
 import csv
+import random
 
 
 class Staff:
@@ -18,40 +19,43 @@ class Venue:
 
 
 def staff_record_handler():  # Read all records related to staff
-    staff_list = []  # List of staff
+    staff_list = {}  # List of staff
+
     for i in range(1, 48):
         newstaff = Staff()
-        newstaff.staff_id = "S" + "{:0>3d}".format(i)
-        staff_list.append(newstaff)
+        new_sid = "S" + "{:0>3d}".format(i)
+        newstaff.staff_id = new_sid
+        staff_list[new_sid] = newstaff
 
     with open('HC04.csv') as staff_unavailable_file:
         su_reader = csv.reader(staff_unavailable_file, delimiter=",")
         line_count = 0
         for row in su_reader:
+            sid = row[0]
             for data in range(len(row)):
                 if data > 0:
-                    staff_list[line_count].unavailable_slot.append(int(row[data]))
+                    staff_list[sid].unavailable_slot.append(int(row[data]))
             line_count += 1
 
     with open('SC01.csv') as consecutive_presentation_file:
         cp_reader = csv.reader(consecutive_presentation_file, delimiter=",")
         line_count = 0
         for row in cp_reader:
-            staff_list[line_count].consecutive_presentation_pref = row[1]
+            staff_list[row[0]].consecutive_presentation_pref = row[1]
             line_count += 1
 
     with open('SC02.csv') as num_day_file:
         nd_reader = csv.reader(num_day_file, delimiter=",")
         line_count = 0
         for row in nd_reader:
-            staff_list[line_count].attend_day = row[1]
+            staff_list[row[0]].attend_day = row[1]
             line_count += 1
 
     with open('SC03.csv') as same_venue_file:
         sv_reader = csv.reader(same_venue_file, delimiter=",")
         line_count = 0
         for row in sv_reader:
-            staff_list[line_count].same_venue_pref = row[1]
+            staff_list[row[0]].same_venue_pref = row[1]
             line_count += 1
 
     return staff_list
@@ -81,30 +85,43 @@ def venue_record_handler():  # Read all records related to venue
         vu_reader = csv.reader(venue_unavailable_file, delimiter=",")
 
         for row in vu_reader:
-            for data in range(len(row)):
-                if data > 0:
-                    venue_list[int(row[data]) - 1].availability = False
+            for data in range(1, len(row)):
+                venue_list[int(row[data]) - 1].availability = False
 
     return venue_list
 
 
 class Presentation:
-    staff_list = []
-    presentation_id = None
-    assigned_venue = None
-
     def __init__(self):
-        print("TBD")
+        self.staff_list = []
+        self.presentation_id = None
+        self.assigned_venue = Venue()
 
 
 class Candidate:
-    presentation_list = []
-
     def __init__(self):
-        print("TBD")
+        self.presentation_list = []
+        self.staff_list = staff_record_handler()
+        with open("SupExaAssign.csv") as SupExaAssign:
+            sea_reader = csv.reader(SupExaAssign, delimiter=",")
+            line_count = 0
 
-    def randomize_venue(self):
-        print("TBD")
+            for row in sea_reader:
+                if line_count > 0:
+                    new_presentation = Presentation()
+                    new_presentation.presentation_id = row[0]
+                    for data in range(1, 3):
+                        new_presentation.staff_list.append(self.staff_list[row[data]])
+                    self.presentation_list.append(new_presentation)
+                line_count += 1
+
+    def randomize_venue(self, venue_list):
+        random_venue_list = []
+        for presentation in self.presentation_list:
+            r = random.randint(1, 300)
+            if presentation.assigned_venue.venue_id != r and r not in random_venue_list:
+                random_venue_list.append(r)
+                presentation.assigned_venue = venue_list[r-1]
 
     def fitness(self):
         print("TBD")
@@ -115,12 +132,23 @@ class Candidate:
 
 class GeneticAlgorithm:
     def __init__(self):
-        print("TBD")
+        self.population = []  # List of candidates
+        self.pop_size = 400
+        self.venue_list = venue_record_handler()
+        new_candidate = Candidate()
+        for i in range(self.pop_size):
+            new_candidate.randomize_venue(self.venue_list)
+            self.population.append(new_candidate)
+        self.population = sorted(self.population, key=lambda candidate: candidate.fitness())  # Sort the population
+
 
     def generate_new_gen(self):
         print("TBD")
 
     def uniform_crossover(self):
+        print("TBD")
+
+    def mutate(self, obj):
         print("TBD")
 
     def run(self):
